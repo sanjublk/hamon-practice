7f14421f2e95357d7e5267f16a248cc579317732
sanju <sanjusk08gmail.com>
2022-08-03 13:52:12 +0530

    removed panagram/panagram.py

--- panagram/panagram.py
+++
@@ -1,8 +0,0 @@
-import string
-
-
-def panagram(s: str) -> bool:
-    """Checks if input string is a panagram"""
-    letters = set(string.ascii_lowercase)  # made a set containing a to z
-    # made a set from input string to checks whether letters is it's subset
-    return set(s.lower()) >= letters
